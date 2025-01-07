2235e33a40bd63c6ff821b557996ef9a827c1f73
dimitris-theca <dimitris.paraschakis@theca.com>
2025-01-07 13:50:24 +0100

    chore: branch cleanup

--- src/eval/experiment.py
+++ src/eval/experiment.py
@@ -33,9 +33,7 @@ class Experiment(ABC):
 
     @staticmethod
     def get_embedder(model_path_or_name: str) -> TextEmbedder:
-        if model_path_or_name.startswith(
-            "/"
-        ):  # local model, needs to be prepended with "/"
+        if model_path_or_name.startswith("/"):  # local model, needs to be prepended with "/"
             model_path_or_name = str(root_dir()) + model_path_or_name
         try:
             return TextEmbedder(model_path_or_name)
@@ -60,9 +58,7 @@ class Experiment(ABC):
         return aux_encoding_schema
 
     def index_products(self, data_vectors: np.array) -> float:
-        print(
-            f"\nData dimensionality: {data_vectors.shape[0]} x {data_vectors.shape[1]}\n"
-        )
+        print(f"\nData dimensionality: {data_vectors.shape[0]} x {data_vectors.shape[1]}\n")
         print(f"Indexing data...\n")
         start_time = time()
         self.client.add(data_vectors)
@@ -73,42 +69,23 @@ class Experiment(ABC):
         if self.client is None:
             self.encoder = self.get_encoder()
             print("Vectorizing data...")
-            data_vectors = self.encoder.encode_products(
-                self.dataset.transformed_df, save_dir=self.dataset.name
-            )
+            data_vectors = self.encoder.encode_products(self.dataset.transformed_df, save_dir=self.dataset.name)
             print("Done.")
             self.client = self.get_client(data_dimensionality=data_vectors.shape[1])
             index_runtime = self.index_products(data_vectors)
             print(f"Indexing time: {index_runtime}")
 
-    def print_experiment(
-        self, res_ids: np.array, res_rel: np.array, random_mods: list[str]
-    ) -> None:
+    def print_experiment(self, res_ids: np.array, res_rel: np.array, random_mods: list[str]) -> None:
         for i in range(len(res_ids)):
             title = f", text: {self.dataset.df.iloc[res_ids[i]][self.main_mod]}, "
-            attributes = ", ".join(
-                [
-                    f"{mod}: {self.dataset.df.iloc[res_ids[i]][mod]}"
-                    for mod in random_mods
-                ]
-            )
-            print(
-                f"Top {i}: id: {res_ids[i]}, rel: {res_rel[i]:.4f}"
-                + title
-                + "{ "
-                + attributes
-                + " }"
-            )
+            attributes = ", ".join([f"{mod}: {self.dataset.df.iloc[res_ids[i]][mod]}" for mod in random_mods])
+            print(f"Top {i}: id: {res_ids[i]}, rel: {res_rel[i]:.4f}" + title + "{ " + attributes + " }")
 
     @abstractmethod
-    def get_results(
-        self, random_id: int, random_mods: list[str], limit: int = 10
-    ) -> (list[dict], float):
+    def get_results(self, random_id: int, random_mods: list[str], limit: int = 10) -> (list[dict], float):
         raise NotImplementedError("Subclasses must implement this method")
 
-    def run_experiment(
-        self, random_id: int, random_mods: list[str], limit: int = 10
-    ) -> (list[dict], float):
+    def run_experiment(self, random_id: int, random_mods: list[str], limit: int = 10) -> (list[dict], float):
         self.init_experiment()
         res_ids, res_rel = self.get_results(random_id, random_mods, limit)
         self.print_experiment(res_ids, res_rel, random_mods)
